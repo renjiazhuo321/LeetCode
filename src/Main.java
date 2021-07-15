56fac0e2f55fe2373f85fb1e58cc08afaeed9b34
renjiazhuo <214370423@qq.com>
2021-07-15 23:39:08 +0800

    offer 04

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,6 @@ import java.util.logging.Logger;
 
 public class Main {
     public static void main(String[] args) {
-        Offer03 offer03 = new Offer03();
-        int a = offer03.findRepeatNumber();
-        System.out.println(a);
+        Offer04 offer03 = new Offer04();
     }
 }
--- src/Offer04.kt
+++ src/Offer04.kt
@@ -2,13 +2,32 @@ class Offer04 {
 
 
     var array = arrayOf(
-        arrayOf
-            (1, 4, 7, 11, 15),
-        arrayOf(2, 5, 8, 12, 19),
-        arrayOf(3, 6, 9, 16, 22),
-        arrayOf(10, 13, 14, 17, 24),
-        arrayOf(18, 21, 23, 26, 30)
+        intArrayOf(1, 4, 7, 11, 15),
+        intArrayOf(2, 5, 8, 12, 19),
+        intArrayOf(3, 6, 9, 16, 22),
+        intArrayOf(10, 13, 14, 17, 24),
+        intArrayOf(18, 21, 23, 26, 30)
     )
 
+    constructor(){
+       val c =  test04(array,4)
+        println(""+c)
+    }
+
+    fun  test04(intArray: Array<IntArray>, target :Int):Boolean{
+        var i = intArray.size - 1
+        var j = 0
+        while (i >= 0 && j < intArray[0].size){
+            if(intArray[i][j]>target){
+                i--
+            }else if(intArray[i][j]<target){
+                j++
+            }else{
+                return true
+            }
+        }
+        return false;
+    }
 
 }
+
